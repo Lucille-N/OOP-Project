dae3929bd620a2d21bdbf8d14b6e669ee1ea8787
Jai063 <carljay437@gmail.com>
2025-10-25 09:44:59 +0800

    Update core.h

--- OOP/core.h
+++ OOP/core.h
@@ -1,1 +1,50 @@
+#include <iostream>
+using namespace std;
 
+class Person (
+public:
+  String name;
+  double salary;
+
+  Person() {
+    name = "";
+    salary = 0;
+  }
+
+  void setData(string n, double s) {
+      name = n;
+      salary = s;
+  }
+
+  void display() {
+      cout << "Name: " << name << ", Salary: " << salary << endl;
+  }
+
+};
+
+class Payroll {
+public:
+  Person employees[10];
+  int count;
+
+  Payroll() {
+    count = 0;
+  }
+
+  void addEmployee(string name, double salary) {
+      if (count < 10) {
+          employees[count].setData(name,salary);
+          count++;
+      } else {
+          cout << "Payroll full!" << endl;
+      }
+  }
+
+}
+
+  void displayAll() {
+      for (int i = 0; i < count; i++)
+          employees[i].display();
+}
+
+};
